193548bb294cf82999488db7aabb7aae5851e41c
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-10-02 19:51:39 -0400

    Complete

---
+++ swift-boat/Boat.swift
@@ -0,0 +1,28 @@
+//
+//  Boat.swift
+//  swift-boat
+//
+//  Created by Bejan Fozdar on 10/2/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+
+class Boat {
+    let name: String
+    var sailors = [String]()
+    var maxSpeedKnots: Double
+    var speedKnots: Double = 0
+    
+    init(name: String, sailors: [String], maxSpeedKnots: Double) {
+        self.name = name
+        self.sailors = sailors
+        self.maxSpeedKnots = maxSpeedKnots
+        }
+    convenience init(name: String, maxSpeedKnots: Double) {
+        self.init(name: name, sailors: [String](), maxSpeedKnots: maxSpeedKnots)
+        }
+    
+}
+
